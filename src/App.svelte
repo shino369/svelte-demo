c7fc3ce2f281579900a18016d924f6b5da06736d
SHINO <nakagame02@gmail.com>
2023-04-29 23:26:24 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Counter from '@/lib/Counter.svelte'
+  import TailwindCss from '@/TailwindCSS.svelte';
+</script>
+
+<main>
+  <TailwindCss />
+  <div class="card w-full h-full">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+
+</style>
