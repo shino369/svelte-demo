502728056325d7cfd8f561ed2583e279d36f830b
SHINO <nakagame02@gmail.com>
2023-04-30 01:58:17 +0900

    example counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import Counter from '@/lib/Counter.svelte'
   import TailwindCss from '@/TailwindCSS.svelte';
+  import Example from './views/Example.svelte';
 </script>
 
 <main>
   <TailwindCss />
   <div class="card w-full h-full">
     <Counter />
+
+    <Example />
   </div>
 
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { fileURLToPath, URL } from "node:url";
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,18 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    emptyOutDir: true,
+    lib: {
+      name: 'svelteDemo',
+      entry: resolve(__dirname, 'index.html'),
+      formats: [ "iife"],
+    },
+    rollupOptions: {
+      // external: ['lodash', 'moment', 'localforage'] // any external library you do not want to include add to here
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
